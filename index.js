287424ebffcfb53928668f7f2cb63fb3ce728a7a
marikuana <43219562+marikuana@users.noreply.github.com>
2019-06-05 17:45:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.onText(/\/test/, (msg, arr) => {
 });
 
 
-  bot.on("callback_query",asyns msg => {
+  bot.on("callback_query",async msg => {
     const data = msg.data.split("_");
     if (data[0] == "weatherCity"){
         usersWeathers[msg.from.id] = {city: data[1]};
